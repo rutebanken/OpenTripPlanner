88ad3d0e89103d9f098a42b307f26d7cdbed2b7f
Gard Mellemstrand <gard.mellemstrand@entur.org>
2020-10-25 13:58:21 +0100

    Filter lines by flex

--- src/ext/java/org/opentripplanner/ext/transmodelapi/TransmodelGraphQLSchema.java
+++ src/ext/java/org/opentripplanner/ext/transmodelapi/TransmodelGraphQLSchema.java
@@ -1314,6 +1314,13 @@ public class TransmodelGraphQLSchema {
                 .description("Set of ids of authorities to fetch lines for.")
                 .type(new GraphQLList(Scalars.GraphQLString))
                 .build())
+            .argument(GraphQLArgument
+                .newArgument()
+                .name("flexibleOnly")
+                .description("Filter by lines containing flexible / on demand serviceJourneys only.")
+                .type(Scalars.GraphQLBoolean)
+                .defaultValue(false)
+                .build())
             .dataFetcher(environment -> {
               if ((environment.getArgument("ids") instanceof List)) {
                 if (environment
@@ -1333,6 +1340,12 @@ public class TransmodelGraphQLSchema {
                     .collect(Collectors.toList());
               }
               Stream<Route> stream = GqlUtil.getRoutingService(environment).getAllRoutes().stream();
+
+              if ((boolean) environment.getArgument("flexibleOnly")) {
+                stream = stream.filter(t -> GqlUtil
+                    .getRoutingService(environment)
+                    .getFlexIndex().routeById.containsKey(t.getId()));
+              }
               if (environment.getArgument("name") != null) {
                 stream = stream
                     .filter(route -> route.getLongName() != null)
